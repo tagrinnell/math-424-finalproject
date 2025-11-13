d3917f64b879cdd1279370706bc00da0ef76d944
tagrinnell <tasman.grinnell@gmail.com>
2025-11-13 09:07:27 -0600

    Still having issues with benchmarking :(

--- benchmark/tuple_vector_bench.cpp
+++ benchmark/tuple_vector_bench.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 #include <tuple>
 #include <cmath>
 #include <chrono>
 
-#define NUM_ITERATIONS 100
+#define NUM_ITERATIONS 1000
 
 long double vector_benchmark() {
-
-    long double total_sum = 0.0;
+    std::chrono::_V2::system_clock::time_point t1, t2;
+    double total_sum = 0.0;
     for (int i = 0; i < NUM_ITERATIONS; i++) {
 
-        auto t1 = std::chrono::high_resolution_clock::now();
+        t1 = std::chrono::high_resolution_clock::now();
 
         std::vector<int> x = {0, 1, 2};
         int y = x[1];
 
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto d = std::chrono::duration<double, std::milli>(t2 - t1);
-        std::cout << d << std::endl;
+        t2 = std::chrono::high_resolution_clock::now();
+        // double x =
+        total_sum += ((const std::chrono::duration<double, std::micro>)(t2 - t1)).count();
+        std::cout << "Iteration " << i << " ran in " << ((const std::chrono::duration<double, std::milli>)(t2 - t1)).count() << " micro-seconds" << std::endl;
     }
 
+    // const std::chrono::duration<double, std::milli> duration = t2 - t1;
+    // std::cout << "Outputting " << duration.count() << "milliseconds" << std::endl;
     return total_sum / (1.0 * NUM_ITERATIONS);
 }
 
@@ -35,19 +39,25 @@ long double tuple_benchmark() {
         int y = std::get<1>(x);
 
         auto t2 = std::chrono::high_resolution_clock::now();
-        total_sum += (t2 - t1).count();
+        total_sum += ((const std::chrono::duration<double, std::milli>)(t2 - t1)).count();
     }
 
     return total_sum / (1.0 * NUM_ITERATIONS);
 }
 
 int main() {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto t2 = std::chrono::high_resolution_clock::now();
 
-    auto tuple_res = tuple_benchmark();
-    auto vec_res = vector_benchmark();
+    const auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    std::cout << "t2 - t1 = " << (int_ms).count() << " milliseconds" << std::endl;
 
-    std::cout << "Tuple ran " << NUM_ITERATIONS << " Iterations in " << tuple_res << "ms" << std::endl;
-    std::cout << "Vector ran " << NUM_ITERATIONS << " Iterations in " << vec_res << "ms" << std::endl;
+    // auto tuple_res = tuple_benchmark();
+    // auto vec_res = vector_benchmark();
+
+    // std::cout << "Tuple ran " << NUM_ITERATIONS << " Iterations averaging " << tuple_res << "ms" << std::endl;
+    // std::cout << "Vector ran " << NUM_ITERATIONS << " Iterations averaging " << vec_res << "ms" << std::endl;
 
     return 0;
 }
